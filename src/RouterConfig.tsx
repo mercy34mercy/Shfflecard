1073df4122cea7d03bc8a73d5b27d8f8d85ff046
mercy34mercy <is0558fs@ed.ritsumei.ac.jp>
2022-08-26 16:23:21 +0900

    ァイル分けたá

--- src/RouterConfig.tsx
+++ src/RouterConfig.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import App from "./App";
-import { Result } from "./Result";
+import App from "./App/App";
+import { Result } from "./Result/Result";
 
 
 export const RouterConfig: React.VFC = () => {
